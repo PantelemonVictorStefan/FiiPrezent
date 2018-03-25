1bda48e1fb8b84d7be06b8a59e04e1f397b76efa
PantelemonVictorStefan <victorboss97@yahoo.com>
2018-03-25 03:36:46 -0700

    Add files via upload

---
+++ FiiPrezent-master/FiiPrezent/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace FiiPrezent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var variable = 0;
+
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+            
+            host.Run();
+        }
+    }
+ }
